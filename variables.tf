38bb16f9cffb9d39f3efcfb21d04ff12fbd245da
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-12 22:59:18 +0000

    updated variable types and descriptions

--- variables.tf
+++ variables.tf
@@ -36,47 +36,58 @@ variable "aws_assume_role_external_id" {
 # -----------------------------------------------------------------------------
 
 variable "enabled" {
-  description = "Whether or not to create the IAM account alias"
-  default     = "true"
+  description = "(Optional) - Whether or not to create the IAM account alias"
+  type        = bool
+  default     = true
 }
 variable "password_policy_enabled" {
-  description = "Whether or not to create the IAM account password policy"
-  default     = "true"
+  description = "(Optional) - Whether or not to create the IAM account password policy"
+  type        = bool
+  default     = true
 }
 variable "allow_users_to_change_password" {
-  description = "Whether to allow users to change their own password"
+  description = "(Optional) - Whether to allow users to change their own password"
+  type        = bool
   default     = true
 }
 variable "hard_expiry" {
-  description = "Whether users are prevented from setting a new password after their password has expired (i.e. require administrator reset)"
+  description = "(Optional) - Whether users are prevented from setting a new password after their password has expired (i.e. require administrator reset)"
+  type        = bool
   default     = false
 }
 variable "max_password_age" {
-  description = "The number of days that a user's password is valid"
+  description = "(Optional) - The number of days that a user's password is valid"
+  type        = number
   default     = 0
 }
 variable "minimum_password_length" {
-  description = "Minimum length to require for user passwords"
+  description = "(Optional) - Minimum length to require for user passwords"
+  type        = number
   default     = 8
 }
 variable "password_reuse_prevention" {
-  description = "The number of previous passwords that users are prevented from reusing"
+  description = "(Optional) - The number of previous passwords that users are prevented from reusing"
+  type        = number
   default     = 10
 }
 variable "require_lowercase_characters" {
-  description = "Whether to require lowercase characters for user passwords"
+  description = "(Optional) - Whether to require lowercase characters for user passwords"
+  type        = bool
   default     = true
 }
 variable "require_uppercase_characters" {
-  description = "Whether to require uppercase characters for user passwords"
+  description = "(Optional) - Whether to require uppercase characters for user passwords"
+  type        = bool
   default     = true
 }
 variable "require_numbers" {
-  description = "Whether to require numbers for user passwords"
+  description = "(Optional) - Whether to require numbers for user passwords"
+  type        = bool
   default     = true
 }
 variable "require_symbols" {
-  description = "Whether to require symbols for user passwords"
+  description = "(Optional) - Whether to require symbols for user passwords"
+  type        = bool
   default     = true
 }
 
